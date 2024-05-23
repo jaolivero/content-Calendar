9bb7d03ebc77a395fae7b34353ca44c8b77110ae
joel olivero <joelolivero@outlook.com>
2024-05-23 01:21:51 -0400

    CRUD Functionality

--- src/main/java/com/mostDev/contentCalendar/controller/ContentController.java
+++ src/main/java/com/mostDev/contentCalendar/controller/ContentController.java
@@ -1,12 +1,61 @@
 package com.mostDev.contentCalendar.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mostDev.contentCalendar.model.Content;
+import com.mostDev.contentCalendar.model.Status;
+import com.mostDev.contentCalendar.model.Type;
+import com.mostDev.contentCalendar.repository.ContentCollectionRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
 
+    private final ContentCollectionRepository repository;
+
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Content not found")) ;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if(!repository.existById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Content not found");
+        }
+        else {
+              repository.save(content);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
 
 }
